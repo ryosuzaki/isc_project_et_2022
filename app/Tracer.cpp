863baf1de1126ef431e1a0d7cced84863029c761
ryosuzaki <71608299+ryosuzaki@users.noreply.github.com>
2022-07-21 17:06:06 +0900

    難所走行追加

--- app/Tracer.cpp
+++ app/Tracer.cpp
@@ -1,6 +1,6 @@
 #include "Tracer.h" // <1>
 Tracer::Tracer():
-  leftWheel(PORT_C), rightWheel(PORT_B), colorSensor(PORT_2) { // <2>
+  leftWheel(PORT_C), rightWheel(PORT_B), colorSensor(PORT_2) ,sonarSensor(PORT_3){ // <2>
   }
 
 void Tracer::init() {
@@ -15,16 +15,66 @@ void Tracer::terminate() {
 
 void Tracer::run() {
   msg_f("running...", 1);
-  high=60;
-  low=30;
+  high=50;
+  low=20;
   mThreshold=20;
-  printf("輝度：%d　カラーナンバー：%d　環境光の強さ：%d\n" , colorSensor.getBrightness(),colorSensor.getColorNumber(),colorSensor.getAmbient());
-  //printf("RGB Raw値：%d" , colorSensor.getRawColor());
-  if(colorSensor.getBrightness() >= mThreshold) { // <1>
-    leftWheel.setPWM(high);
-    rightWheel.setPWM(low);
-  } else {   // <2>
-    leftWheel.setPWM(low);
-    rightWheel.setPWM(high);
+
+  //flg 0=通常 1以降=難所
+  if(flg==0){
+    printf("輝度：%d　カラーナンバー：%d　環境光の強さ：%d\n" , colorSensor.getBrightness(),colorSensor.getColorNumber(),colorSensor.getAmbient());
+    //難所探知機能 緑を検出したらflgを1に変更
+    if(colorSensor.getColorNumber()==3){
+      leftWheel.setPWM(-30);
+      rightWheel.setPWM(-30);
+      flg=1;
+    }
+
+    //****************************
+    //ここに通常ルートのコードを書く
+    if(colorSensor.getBrightness() >= mThreshold) { // <1>
+      leftWheel.setPWM(high);
+      rightWheel.setPWM(low);
+    }else {   // <2>
+      leftWheel.setPWM(low);
+      rightWheel.setPWM(high);
+    }
+    //ここまで通常ルートのコードを書く
+    //******************************
+
+  }else if(flg==1){
+    //
+    leftWheel.setPWM(0);
+    rightWheel.setPWM(20);
+    if (sonarSensor.getDistance()<100){
+      flg=2;
+      pre_distance=sonarSensor.getDistance();
+    }
+  }else if(flg==2){
+    //方向転換
+    rightWheel.setPWM(-5);
+    if (pre_distance<sonarSensor.getDistance()){
+      flg=3;
+    }
+    pre_distance=sonarSensor.getDistance();
+    printf("%d\n",sonarSensor.getDistance());
+  }else if(flg==3){
+    //ブロック位置合わせ
+    count+=1;
+    rightWheel.setPWM(3);
+    if(count==6){
+      flg=4;
+    }
+    printf("%d\n",count);
+  }else if(flg==4){
+    //高得点位置まで移動
+    leftWheel.setPWM(30);
+    rightWheel.setPWM(30);
+    if(sonarSensor.getDistance()<18){
+      leftWheel.setPWM(0);
+      rightWheel.setPWM(0);
+      flg=5;
+    }
+    printf("%d\n",sonarSensor.getDistance());
   }
+  
 }
--- app/Tracer.h
+++ app/Tracer.h
@@ -2,6 +2,7 @@
 #include "ColorSensor.h" // <2>
 #include "util.h"        // <3>
 #include "Clock.h"
+#include "SonarSensor.h"
 
 using namespace ev3api;  // <4>
 
@@ -15,10 +16,16 @@ public:
 private:
   Motor leftWheel;
   Motor rightWheel;
+  Motor tailWheel;
   ColorSensor colorSensor; // <3>
+  SonarSensor sonarSensor;
   int8_t mThreshold = 20;  // <4>
-  int8_t high=60;
-  int8_t low=30;
+  int8_t flg=0;// 0=通常 1=難所
+  int8_t high=0;
+  int8_t low=0;
+
+  int8_t pre_distance=0;
+  int8_t count=0;
 /*#ifndef MAKE_RASPIKE
   int8_t pwm = (Motor::PWM_MAX) / 6;
 #else
