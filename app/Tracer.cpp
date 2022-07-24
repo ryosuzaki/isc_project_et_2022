7111f8445bb1cb6a3ebe7075a45df00585ac39eb
ryosuzaki <71608299+ryosuzaki@users.noreply.github.com>
2022-07-24 15:37:06 +0900

    fix

--- app/Tracer.cpp
+++ app/Tracer.cpp
@@ -15,23 +15,20 @@ void Tracer::terminate() {
 
 void Tracer::run() {
   msg_f("running...", 1);
-  high=50;
-  low=20;
+  //
   mThreshold=20;
-
-  //flg 0=通常 1以降=難所
-  if(flg==0){
-    printf("輝度：%d　カラーナンバー：%d　環境光の強さ：%d\n" , colorSensor.getBrightness(),colorSensor.getColorNumber(),colorSensor.getAmbient());
-    //難所探知機能 緑を検出したらflgを1に変更
-    if(colorSensor.getColorNumber()==3){
-      leftWheel.setPWM(-30);
-      rightWheel.setPWM(-30);
-      flg=1;
-    }
-
+  //
+  brightness=colorSensor.getBrightness();
+  color_number=colorSensor.getColorNumber();
+  ambient=colorSensor.getAmbient();
+  distance=sonarSensor.getDistance();
+  printf("輝度：%d　カラーナンバー：%d　環境光の強さ：%d　距離：%d\n",brightness,color_number,ambient,distance);
+  
+  //state 0は通常 1以降は難所
+  if(state==0){
     //****************************
     //ここに通常ルートのコードを書く
-    if(colorSensor.getBrightness() >= mThreshold) { // <1>
+    if(brightness >= mThreshold) { // <1>
       leftWheel.setPWM(high);
       rightWheel.setPWM(low);
     }else {   // <2>
@@ -41,40 +38,59 @@ void Tracer::run() {
     //ここまで通常ルートのコードを書く
     //******************************
 
-  }else if(flg==1){
-    //
+    //青い線を通過したら、passed_blueを1に変更
+    if(color_number==2){
+      passed_blue=1;
+      high=40;
+      low=20;
+      printf("青い線を通過\n");
+    }
+    
+    //難所探知機能 緑を検出したらstateを1に変更
+    if(color_number==3){
+      leftWheel.setPWM(-30);
+      rightWheel.setPWM(-30);
+      state=1;
+      printf("難所探知\n");
+    }
+
+  }else if(state==1){
+    //左に回転
     leftWheel.setPWM(0);
     rightWheel.setPWM(20);
-    if (sonarSensor.getDistance()<100){
-      flg=2;
-      pre_distance=sonarSensor.getDistance();
+    if (distance<98){
+      state=2;
+      pre_distance=distance;
     }
-  }else if(flg==2){
+    printf("state=1 左に回転:%d\n",distance);
+  }else if(state==2){
     //方向転換
     rightWheel.setPWM(-5);
-    if (pre_distance<sonarSensor.getDistance()){
-      flg=3;
+    if (pre_distance<distance){
+      state=3;
     }
-    pre_distance=sonarSensor.getDistance();
-    printf("%d\n",sonarSensor.getDistance());
-  }else if(flg==3){
+    pre_distance=distance;
+    printf("state=2 右に回転:%d\n",distance);
+  }else if(state==3){
     //ブロック位置合わせ
-    count+=1;
-    rightWheel.setPWM(3);
     if(count==6){
-      flg=4;
+      state=4;
+    }else{
+      count+=1;
+      rightWheel.setPWM(3);
+      printf("state=3 ブロック位置合わせ回数:%d\n",count);
     }
-    printf("%d\n",count);
-  }else if(flg==4){
+  }else if(state==4){
     //高得点位置まで移動
-    leftWheel.setPWM(30);
-    rightWheel.setPWM(30);
-    if(sonarSensor.getDistance()<18){
+    if(distance<20){
       leftWheel.setPWM(0);
       rightWheel.setPWM(0);
-      flg=5;
+      printf("停止\n");
+      state=5;
+    }else{
+      leftWheel.setPWM(30);
+      rightWheel.setPWM(30);
+      printf("state=4 壁までの距離:%d\n",distance);
     }
-    printf("%d\n",sonarSensor.getDistance());
   }
-  
 }
--- app/Tracer.h
+++ app/Tracer.h
@@ -18,11 +18,23 @@ private:
   Motor rightWheel;
   ColorSensor colorSensor; // <3>
   SonarSensor sonarSensor;
-  int8_t mThreshold = 20;  // <4>
-  int8_t flg=0;// 0=通常 1=難所
-  int8_t high=0;
-  int8_t low=0;
 
+  //
+  int8_t mThreshold = 20;  // <4>
+
+  //初期モータースピード
+  int8_t high=50;
+  int8_t low=20;
+
+  //
+  int8_t color_number=0;
+  int8_t brightness=0;
+  int8_t ambient=0;
+  int16_t distance=0;
+
+  int8_t passed_blue=0;//青い線を通過したか
+  int8_t state=0;//0は通常 1以降は難所
+  
   int8_t pre_distance=0;
   int8_t count=0;
 /*#ifndef MAKE_RASPIKE
